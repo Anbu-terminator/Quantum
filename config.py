eb242a64e3cb62c7eed574e2404581fbc1521808
Anbu Sivam.B <anbulegend101@gmail.com>
2025-10-05 20:45:50 +0530

    config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 # server/config.py
-# -------------------
-# ⚠️ Keep this file secret – contains all API and encryption credentials.
-
 IBM_API_TOKEN = "iFQsHF7gsW7qbJGV2tWNQmL4zW9dVjl8wkStMt6m0sS4"
-MONGODB_URI = "mongodb+srv://bastoffcial:aI4fEcricKXwBZ4f@speedo.swuhr8z.mongodb.net/"
 
 THINGSPEAK_CHANNEL_ID = "3031584"
 THINGSPEAK_READ_KEY   = "T9VU037H0SE1PG8U"
@@ -12,7 +8,7 @@ THINGSPEAK_WRITE_KEY  = "VGC3K4HUZBM7ABFZ"
 ESP_AUTH_TOKEN = "6772698c38270a210fabf1133fc6ad00"
 
 KEY_ROTATE_SECONDS = 60
-KEEP_KEYS = 10
+KEEP_KEYS = 1000  # Keep more keys for historical mapping
 
 SERVER_AES_KEY_HEX = "00112233445566778899aabbccddeeff"
 
