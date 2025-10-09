29ab10d71cc1b4c04eb2c6bac954bdbf13d87806
Anbu Sivam.B <anbulegend101@gmail.com>
2025-10-09 18:49:13 +0530

    quantum_key.py

---
+++ quantum_key.py
@@ -0,0 +1,6 @@
+# quantum_key.py
+import random, string
+
+def get_quantum_challenge():
+    # Simulate random token
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(32))
