cc698cfebaf29bec6e212038ad6c8b5b91650cff
Anbu Sivam.B <anbulegend101@gmail.com>
2025-10-18 13:01:59 +0530

    config.py

--- config.py
+++ config.py
@@ -1,20 +1,11 @@
 # backend/config.py
-
-# HMAC key used by device for HMAC-SHA256
 IBM_API_TOKEN = "iFQsHF7gsW7qbJGV2tWNQmL4zW9dVjl8wkStMt6m0sS4"
-
-# ThingSpeak settings
 THINGSPEAK_CHANNEL_ID = "3100917"
-THINGSPEAK_READ_KEY = "AT5M7WZ9WQX31AHN"
-
-# AES settings (must match ESP)
-SERVER_AES_KEY_HEX = "00112233445566778899aabbccddeeff"
-AES_IV_HEX = "000102030405060708090a0b0c0d0e0f"
-
-# Optional simple auth token to protect /feeds/latest (device/frontend can include this)
+THINGSPEAK_READ_KEY   = "AT5M7WZ9WQX31AHN"
+THINGSPEAK_WRITE_KEY  = "4ONVDAM8LZD1KGH0"
 ESP_AUTH_TOKEN = "6772698c38270a210fabf1133fc6ad00"
-
-# How many results to request from ThingSpeak
-THINGSPEAK_RESULTS = 1
-IBM_Q_BACKEND = "ibmq_qasm_simulator"
-IBM_Q_SHOTS = 256
+# 128-bit AES key (16 bytes) in hex (provided by you)
+SERVER_AES_KEY_HEX = "00112233445566778899aabbccddeeff"
+# Flask server settings
+FLASK_HOST = "0.0.0.0"
+FLASK_PORT = 5000
